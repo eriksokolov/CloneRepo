f53bab082937af0675b004fbd819f3322d04ffa2
erik <eriksokolov@outlook.com>
2026-03-23 22:36:56 +0100

    03/23/2026 22:36:56

--- CloneRepo.ps1
+++ CloneRepo.ps1
@@ -3,6 +3,6 @@ $ImportCSV = (Import-csv .\*.csv)
 
 foreach ($import in $ImportCSV)
 {
-  git clone "https://github.com/eriksokolov/$($import.repo)" "$env:computerprofile\repos\$($import.repo)"
+  git clone "https://github.com/eriksokolov/$($import.repo)" "$env:userprofile\repos\$($import.repo)"
 }
 
